7204cd9059fc802fe43e73429cafb782e3b510bf
navio-x <navioxwallet@gmail.com>
2026-05-03 16:52:52 +0300

    Update wallets.vue

--- src/views/wallets.vue
+++ src/views/wallets.vue
@@ -124,7 +124,7 @@
       <div class="relative rounded-xl shadow-2xl glass-modal w-full max-w-lg mx-4 p-6">
         <div class="flex items-start justify-between mb-5">
           <h3 class="text-xl font-semibold text-white">Rescan Blockchain</h3>
-          <button @click="showRescanModal = false" class="text-white/40 hover:text-white/80 transition-colors focus:outline-none">
+          <button @click="showRescanModal = false" class="text-white/40 hover:text-white/80 bg-transparent rounded-lg p-1.5 ml-auto inline-flex items-center transition-colors focus:outline-none">
             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
           </button>
         </div>
